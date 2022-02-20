8d01156874eb9d36255859531e5051734ca96188
ErrorDa <99288583+ErrorDa@users.noreply.github.com>
2022-02-20 21:19:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -10,7 +10,7 @@ bool checkPrime(uint64_t value) {
   for (d = 2; d * d <= value; d++) {
     if (value % d == 0)
       return false;
-	  }
+  }
     return true;
 }
 
@@ -18,50 +18,50 @@ uint64_t nPrime(uint64_t n) {
   int d, k, value;
   bool flag = true;
   value = 2;
-  for (k = 0;n != k;) {
+  for (k = 0 ;n != k ; ) {
     for (d = 2; d * d <= value; d++) {
       if (value % d == 0) {
         flag = false;
         break;
-		  }
-	  }
-	  if (flag)
-		  k += 1;
-	  value += 1;
+      }
+    }
+    if (flag)
+      k += 1;
+    value += 1;
     flag = true;
   }
   return value - 1;
 }
 
 uint64_t nextPrime(uint64_t value) {
-	int d;
-	for (d = 2; d * d <= value; d++) {
-	  if (value % d == 0) {
-			value += 1;
-			d = 1;
-		}
-	}
-	return value;
+  int d;
+  for (d = 2; d * d <= value; d++) {
+    if (value % d == 0) {
+      value += 1;
+      d = 1;
+    }
+  }
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
   int d, k, i, s;
-	s = 0;
-	int* resh;
-	resh = new int[hbound];
+  s = 0;
+  int* resh;
+  resh = new int[hbound];
   resh[1] = 0;
   for (i = 2; i <= hbound; i++)
     resh[i] = 1;
-		for (d = 2; d * d <= hbound; d++) {
-		  if (resh[d] == 1) {
-			  for (k = d * d; k <= hbound; k += d) {
-					resh[k] = 0;
-				}
-			}
-		}
+    for (d = 2; d * d <= hbound; d++) {
+      if (resh[d] == 1) {
+        for (k = d * d; k <= hbound; k += d) {
+          resh[k] = 0;
+        }
+      }
+    }
   for (i = 0;i < hbound;i++) {
-	  if (resh[i])
-		  s += i;
-	}
-	return s;
+    if (resh[i])
+      s += i;
+  }
+  return s;
 }
